1979aa2d8277b6bc4d31a542f7362a0e1c9bdc94
anulmehta48 <anulmehta1848@gmail.com>
2022-09-05 16:09:31 +0530

    create

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const route = require('./router/route.js');
+const monogoose = require('mongoose');
+const app = express();
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
+
+monogoose.connect('mongodb+srv://funupdb-first:VxaFh8Uez4zyv95l@cluster0.kizeuyb.mongodb.net/project1?retryWrites=true&w=majority', {
+    useNewParser: true
+})
+
+    .then(() => console.log("MongoDB is Connected"))
+    .catch(err => console.log(err))
+
+app.use("/", route);
+
+
+app.listen(process.env.PORT || 3000, () => { console.log("express running on port" + (process.env.PORT || 3000)) });
