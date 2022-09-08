9eb62935164db1f80b306baf5fcda66df2e26339
AakashGhosh7044384754 <109957580+AakashGhosh7044384754@users.noreply.github.com>
2022-09-09 00:58:17 +0530

    Delete src/validation directory

--- src/validation/validation.js
+++
@@ -1,6 +0,0 @@
-const authorEmail = (value) => {
-    const emailRegex = /\b[\w\.-]+@[\w\.-]+\.\w{2,4}\b/gi;
-      if (!emailRegex.test(value.trim())) return false;
-      return true;
-    };
-    module.exports.authorEmail=authorEmail
